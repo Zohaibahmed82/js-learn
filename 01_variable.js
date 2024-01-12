51ae449f0c4f4e1870a01d7013a0307bac6178b8
zohaibahmed <officerbhia123@gmail.com>
2024-01-13 01:24:54 +0500

    variable data types

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 12344;
+let accountEmail = "Zohaibahmed@gmail.com";
+var accountPassword = "1234455";
+accountCity = "Swabi";
+let accountName;
+// accountId = 12345; not allowed because we can not change a const value const value can not be changed
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountName]);
+
+// not preferd var because it can block scope and functional scope
