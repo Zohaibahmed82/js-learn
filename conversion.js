af3edec1a31c706316bcb0c1bff9563ffc972864
zohaibahmed <officerbhia123@gmail.com>
2024-01-14 23:47:53 +0500

    conversion

---
+++ conversion.js
@@ -0,0 +1,17 @@
+// let Data = "3333";
+// console.log(typeof Data);
+
+// let Isnumber = Number(Data);
+// console.log(typeof(Isnumber));
+// Note When we have pass a data in string
+//  we can esaly convert in number formate But we can
+//   full sure the data is coming in digits form if not coming it can create some issues
+// let Data = "3323dd";
+// let Isnumber = Number(Data);
+// console.log(typeof(Isnumber));
+// console.log(Isnumber);
+// let data2 = parseInt(Data); // parseInt can return A NUMBER mean when we can come data in one column string and number mixed so this can return me only a number
+// console.log(data2);
+
+/* we have multiple conversion so we can used one by one
+so in this time we can move in operational method */
